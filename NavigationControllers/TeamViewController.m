362832ba5c0dd8f427a853f2d32b6968f3ddbfa7
chriswebb09 <chris.webb5249@gmail.com>
2016-06-20 15:59:52 -0400

    Code edit

--- NavigationControllers/TeamViewController.m
+++ NavigationControllers/TeamViewController.m
@@ -15,32 +15,41 @@
 {
     
     if ([segue.identifier isEqualToString:@"alSegue"]) {
-        self.teamMemberToPass = [[TeamMember alloc]initWithName:@"Al"
-                                                    PhoneNumber:@"555555"
-                                                          Photo:@"al"
-                                                      BirthCity:@"Detroit"
-                                                     BirthState:@"MI" FavoriteBand:@"Beatles"];
+        
+        self.teamMemberToPass = [[TeamMember alloc] initWithName:@"Al"
+                                                     PhoneNumber:@"555555"
+                                                           Photo:@"al"
+                                                       BirthCity:@"Detroit"
+                                                      BirthState:@"MI"
+                                                    FavoriteBand:@"Beatles"];
         
     } else if ([segue.identifier isEqualToString:@"aviSegue"]) {
-        self.teamMemberToPass = [[TeamMember alloc]initWithName:@"Avi"
-                                                    PhoneNumber:@"555555"
-                                                          Photo:@"avi"
-                                                      BirthCity:@"New York" BirthState:@"NY" FavoriteBand:@"Led Zepplin"];
+        
+        self.teamMemberToPass = [[TeamMember alloc] initWithName:@"Avi"
+                                                     PhoneNumber:@"555555"
+                                                           Photo:@"avi"
+                                                       BirthCity:@"New York"
+                                                      BirthState:@"NY"
+                                                    FavoriteBand:@"Led Zepplin"];
+        
     } else if ([segue.identifier isEqualToString:@"joeSegue"]) {
-        self.teamMemberToPass = [[TeamMember alloc]initWithName:@"Joe"
-                                                    PhoneNumber:@"555555"
-                                                          Photo:@"joe"
-                                                      BirthCity:@"Madison"
-                                                     BirthState:@"WI"
-                                                   FavoriteBand:@"Backstreet Boys"];
+        
+        self.teamMemberToPass = [[TeamMember alloc] initWithName:@"Joe"
+                                                     PhoneNumber:@"555555"
+                                                           Photo:@"joe"
+                                                       BirthCity:@"Madison"
+                                                      BirthState:@"WI"
+                                                    FavoriteBand:@"Backstreet Boys"];
         
     } else if ([segue.identifier isEqualToString:@"chrisSegue"]) {
-        self.teamMemberToPass = [[TeamMember alloc]initWithName:@"Chris"
-                                                    PhoneNumber:@"555555"
-                                                          Photo:@"chris"
-                                                      BirthCity:@"Los Angeles"
-                                                     BirthState:@"CA"
-                                                   FavoriteBand:@"Linkin Park"];
+        
+        self.teamMemberToPass = [[TeamMember alloc] initWithName:@"Chris"
+                                                     PhoneNumber:@"555555"
+                                                           Photo:@"chris"
+                                                       BirthCity:@"Los Angeles"
+                                                      BirthState:@"CA"
+                                                    FavoriteBand:@"Linkin Park"];
+        
     }
     
     TeamDetailViewController *teamDetailVC = [segue destinationViewController];
@@ -50,6 +59,7 @@
     teamDetailVC.passedPhoneNumber = self.teamMemberToPass.phoneNumber;
     teamDetailVC.passedBirthPlace = self.teamMemberToPass.birthPlace;
     teamDetailVC.passedFavoriteBand = self.teamMemberToPass.favoriteBand;
+    
 }
 
 @end
